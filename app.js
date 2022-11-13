b74780bd3aa7b00dcae597aae0d2219a0a827acc
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-13 20:05:42 +0200

    security setup

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // connectDB
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
@@ -14,8 +19,17 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 mins
+    max: 100, // limit each IP to 100 request per windowMs
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
